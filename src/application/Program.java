83e2855f1c513c1d00f2d68136e52844cecc1f18
Arthur Estrela <arthurestrela369@gmail.com>
2024-07-04 14:59:31 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import modelEntites.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
